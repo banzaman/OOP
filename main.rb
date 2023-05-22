5553703dca53fb0c4d0502c7c531f718dfac76a9
Giancarlo Dumani <gdumani@gmail.com>
2023-05-22 14:52:20 -0600

    move menu choice from main to menu

--- main.rb
+++ main.rb
@@ -1,11 +1,9 @@
-# rubocop:disable Metrics/CyclomaticComplexity
-
 require_relative './app'
 require_relative './menu'
 
 def main
   app = App.new
-  menu = Menu.new
+  menu = Menu.new app
 
   puts '
   Welcome to School Library App!'
@@ -13,20 +11,9 @@ def main
   loop do
     menu.display_options
     choice = gets.chomp.to_i
-
-    case choice
-    when 1 then app.list_books
-    when 2 then app.list_people
-    when 3 then app.create_person
-    when 4 then app.create_book
-    when 5 then app.create_rental
-    when 6 then app.list_rentals
-    when 7 then puts 'Thanks for using this app!'
-                exit
-    end
+    abort('Thanks for using this app!') if choice == menu.menu_exit
+    menu.choose choice
   end
 end
 
 main
-
-# rubocop:enable Metrics/CyclomaticComplexity
--- menu.rb
+++ menu.rb
@@ -1,4 +1,11 @@
 class Menu
+  attr_reader :menu_exit
+
+  def initialize(app)
+    @menu_exit = 7
+    @app = app
+  end
+
   def display_options
     puts '
         Please choose an option by entering a number
@@ -10,5 +17,16 @@ class Menu
           5 - Create a rental
           6 - List all rentals for a given person id
           7 - Exit'
+  end
+
+  def choose(choice)
+    case choice
+    when 1 then @app.list_books
+    when 2 then @app.list_people
+    when 3 then @app.create_person
+    when 4 then @app.create_book
+    when 5 then @app.create_rental
+    when 6 then @app.list_rentals
+    end
   end
 end
