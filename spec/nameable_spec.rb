d786b8ffb7a4cf21cd7820ff251b66030a9c8db7
Giancarlo Dumani <gdumani@gmail.com>
2023-05-25 07:45:40 -0600

    Create nameable_spec.rb

---
+++ spec/nameable_spec.rb
@@ -0,0 +1,21 @@
+require_relative '..\nameable'
+
+class Person_without_correct_name < Nameable
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    super()
+  end
+end
+
+describe Nameable do
+  it 'check allert when correct_name method not present in class' do
+    person = Person.new(33, 'John')
+    expect { person.correct_name }.to raise_error('You must implement the correct_name method')
+  end
+end
